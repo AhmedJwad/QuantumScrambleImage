6e8115ce3eeded7d321cd7347328eb864953bdcd
AhmedJwad <Ahmednet380@gmail.com>
2023-12-21 22:46:41 -0800

    convert image to quantumstate

--- QuantumScrambleImage/QuantumScrambleImage/Controllers/ImagesController.cs
+++ QuantumScrambleImage/QuantumScrambleImage/Controllers/ImagesController.cs
@@ -4,7 +4,20 @@ using QuantumScrambleImage.Data;
 using QuantumScrambleImage.Data.Entities;
 using QuantumScrambleImage.Helpers;
 using QuantumScrambleImage.Models;
+using System.Drawing.Imaging;
+using System.Drawing;
 using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Numerics;
+using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using static System.Net.Mime.MediaTypeNames;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp;
+using Image = SixLabors.ImageSharp.Image;
+using Color = System.Drawing.Color;
 
 namespace QuantumScrambleImage.Controllers
 {
@@ -41,7 +54,7 @@ namespace QuantumScrambleImage.Controllers
             if(ModelState.IsValid) 
             {
                 string ImageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "ImagesEncrypted");
-                Image image = new()
+                Data.Entities.Image image = new()
                 {
                     ImageId = ImageId,
                     isEncrypted = false,
@@ -62,7 +75,7 @@ namespace QuantumScrambleImage.Controllers
                 return NotFound();
             }
 
-           Image image = await _context.Images
+           Data.Entities.Image image = await _context.Images
                 .FirstOrDefaultAsync(x => x.Id == Id);
             if (image == null)
             {
@@ -79,5 +92,77 @@ namespace QuantumScrambleImage.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+
+        public async Task<IActionResult> Scrambleimage(int? id)
+        {
+            if(id==null)
+            {
+                return NotFound();
+            }
+            Data.Entities.Image image = await _context.Images
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (image == null) 
+            {
+                return NotFound();
+            }
+            var httpClient = new HttpClient();
+            var stream = await httpClient.GetStreamAsync(image.ImageFullPath);
+            Bitmap inputImage = new Bitmap(stream);
+
+            string Quantumsate = ConvertToQuantumState(inputImage);
+
+            Bitmap outputImage = ConvertToImage(Quantumsate, inputImage.Width, inputImage.Height);
+                // Display the scrambled image          
+            outputImage.Save(Path.Combine(Directory.GetCurrentDirectory(), $"wwwroot\\images\\scrampleimage\\scrambledimages" + ".jpg"));
+            return RedirectToAction (nameof(Index));
+        }
+        public  string ConvertToQuantumState(Bitmap bitmap)
+        {
+            int width = bitmap.Width;
+            int height = bitmap.Height;
+
+            StringBuilder quantumState = new StringBuilder();
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Color pixelColor = bitmap.GetPixel(j, i);
+
+                    string binaryPixel = Convert.ToString(pixelColor.R, 2).PadLeft(8, '0') +
+                                         Convert.ToString(pixelColor.G, 2).PadLeft(8, '0') +
+                                         Convert.ToString(pixelColor.B, 2).PadLeft(8, '0');
+
+                    quantumState.Append(binaryPixel);
+                }
+            }
+
+            return quantumState.ToString();
+        }
+        public  Bitmap ConvertToImage(string quantumState, int width, int height)
+        {
+            Bitmap result = new Bitmap(width, height);
+
+            int index = 0;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    string pixelData = quantumState.Substring(index, 24);
+                    index += 24;
+
+                    int r = Convert.ToInt32(pixelData.Substring(0, 8), 2);
+                    int g = Convert.ToInt32(pixelData.Substring(8, 8), 2);
+                    int b = Convert.ToInt32(pixelData.Substring(16, 8), 2);
+
+                    Color pixelColor = Color.FromArgb(r, g, b);
+                    result.SetPixel(j, i, pixelColor);
+                }
+            }
+
+            return result;
+        }
+
     }
 }
---
+++ QuantumScrambleImage/QuantumScrampleImage.Common/Enums/QuantumGateType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumScrampleImage.Common.Enums
+{
+    public enum QuantumGateType
+    {
+        CNOT,
+        Swap,
+    }
+}
